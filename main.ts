228cb590d33e99d00c5f2c0d5c81f1b00e2cab96
AqeeAqee <aqee@msn.com>
2023-09-26 02:28:48 +0800

    clean up main.ts

--- main.ts
+++ main.ts
@@ -1,79 +1,43 @@
 
 game.stats = true
-const rcRender = Render.raycastingRender
-Render.moveWithController(0, 0, 0)
-
-controller.moveSprite(rcRender.sprSelf, 66, 0)
-
-// let trans16 = image.create(16, 16)
-
-
-const tm = tiles.createTilemap(hex`1000100003040506070102010201020102010201010201020102010201020102010201020201020102010201020102010201020101020102010201020102000201020102020102010201020102010201020102010102010201020102010201020102010202010201020102010201020102010201010201020102010201020102010201020201020102010201020102010201020101020102010201020102010201020102020102010201020102010201020102010102010201020102010201020102010202010201020102010201020102010201010201020102010201020102010201020201020102010201020102010201020101020102010201020102010201020102`, img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . 2 2 2 2 . . 2 2 2 2 . . .
-    . . . 2 . . . . . . . . 2 . . .
-    . . . 2 . . . . . . . . 2 . . .
-    . . . 2 . . . . . . . . 2 . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . 2 . . . . . . . . 2 . . .
-    . . . 2 . . . . . . . . 2 . . .
-    . . . 2 . . . . . . . . 2 . . .
-    . . . 2 2 2 2 . . 2 2 2 2 . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`, [myTiles.transparency16,myTiles.tile3,myTiles.tile4,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal,sprites.dungeon.chestClosed], TileScale.Sixteen);
-// tiles.setCurrentTilemap(tm)
-// const spawnTile = myTiles.tile4
-
-// tiles.setCurrentTilemap(tilemap`level1`)
-// const spawnTile = sprites.castle.tileDarkGrass3
 
 tiles.setCurrentTilemap(tilemap`level0`)
 const spawnTile = sprites.castle.tileGrass1
+// effects.blizzard.startScreenEffect(99999999, 99)
 
-const tilemapScale = 1 << game.currentScene().tileMap.scale
-rcRender.sprSelf.setPosition(1 * tilemapScale, 8 * tilemapScale)
-rcRender.sprSelf.ay=500
-
+const rcRender = Render.raycastingRender
 let angle = 45
 Render.setViewAngleInDegree(angle)
+Render.moveWithController(0, 0, 0)
+controller.moveSprite(rcRender.sprSelf, 55, 0)
+
+rcRender.sprSelf.setScale(1)
+rcRender.sprSelf.ay=500
+tiles.placeOnTile(rcRender.sprSelf, tiles.getTileLocation(2, 8))
 Render.setSpriteAnimations(rcRender.sprSelf, Render.createAnimations(150, texturesHero[0], texturesHero[1], texturesHero[2], texturesHero[3]))
-
-
-// effects.blizzard.startScreenEffect(99999999, 99)
 
 let count = 0
 function createSprite(x: number, y: number, vx: number, vy: number, textures: Image[][], kind: number) {
     const spr = sprites.create(textures[0][0], kind)
     // rcRender.takeoverSceneSprites()
-    const tilemapScale = 1 << game.currentScene().tileMap.scale
-    spr.setPosition(x * tilemapScale, y * tilemapScale)
+    tiles.placeOnTile(spr, tiles.getTileLocation(x, y))
     spr.setVelocity(vx, vy)
-    // spr.setBounceOnWall(true)
-    spr.setScale(0.5)
     // setCharacterAnimationForSprite(spr, textures)
     Render.setSpriteAnimations(spr, Render.createAnimations(150, textures[0], textures[1], textures[2], textures[3]))
-    // if (kind == SpriteKind.Enemy)
-    //     tiles.placeOnRandomTile(spr, trans16)
-    spr.sayText(spr.id + " test\n test", 9999)
-    // spr.startEffect(effects.fountain,9999)
-
-    //isometric side View
-    spr.setBounceOnWall(false)
+    tiles.placeOnRandomTile(spr, spawnTile)
     spr.ay = 500
 
+    // spr.sayText(spr.id + " test\n test", 9999)
+    // spr.startEffect(effects.fountain,9999)
 
     return spr
 }
 
-// 0<= dir <1, then may be added by 2 for avoid negative
-// rcRender.registerOnSpriteDirectionUpdate((spr, dir) => {
-//     // character.setCharacterState(spr, character.rule(characterAniDirs[Math.floor(dir * 4 + .5) % 4]))
-// })
+function createCoin() {
+    let spr = createSprite(4, 7, Math.randomRange(5, 10), Math.randomRange(3, 10), texturesCoin, SpriteKind.Food)
+    tiles.placeOnRandomTile(spr, spawnTile)
+    rcRender.setZOffset(spr, .5)
+}
 
 // createSprite(8, 7, 6, 10, texturesDuck, SpriteKind.Enemy)
 // createSprite(6, 7, 6, 10, texturesDonut, SpriteKind.Enemy)
@@ -86,165 +50,95 @@ let sprPlane = createSprite(9, 7, 6, 10, texturesPlane, SpriteKind.Enemy)
 // let cake = createSprite(2, 2, 4, 2, texturesBigCake, SpriteKind.Enemy)
 let fish = createSprite(7, 9, 0, 0, texturesFish, SpriteKind.Enemy)
 
-sprSkelly.startEffect(effects.fire, 9999999)
-
-Render.setSpriteAttribute(sprSkelly, RCSpriteAttribute.ZOffset, 4)
-Render.setSpriteAttribute(sprPlane, RCSpriteAttribute.ZOffset, 16)
-// Render.setSpriteAttribute(cake, RCSpriteAttribute.ZOffset, 4)
-Render.setSpriteAttribute(fish, RCSpriteAttribute.ZOffset, 8)
-
-function createCoin() {
-    let spr = createSprite(4, 7, Math.randomRange(5, 10), Math.randomRange(3, 10), texturesCoin, SpriteKind.Food)
-    tiles.placeOnRandomTile(spr, spawnTile)
-    rcRender.setZOffset(spr, .25)
-}
-
-//test for RelativeToCamera
-// game.onUpdateInterval(3000, ()=>{
-//     cake.setFlag(SpriteFlag.RelativeToCamera, !(cake.flags&SpriteFlag.RelativeToCamera))
-// })
-
-controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
-    music.pewPew.play()
-    let s = sprites.createProjectileFromSprite(sprites.projectile.bubble1, rcRender.sprSelf, 55, 0)
-    s.setScale(0.25)
-    rcRender.setZOffset(s, rcRender.getMotionZPosition(rcRender.sprSelf) + 2)  //todo, use VP height
-
-    const onTheWall = tiles.tileAtLocationIsWall(rcRender.sprSelf.tilemapLocation())
-    info.player4.setScore(onTheWall ? 999 : 0)
-    if (onTheWall) {
-        s.flags |= sprites.Flag.IsClipping;
-    }
-})
-
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
-    if (!rcRender.isOverlapZ(sprite, otherSprite)) return
-
-    sprite.setVelocity(0, 0)
-    sprite.sayText("No!", 2000)
-    rcRender.move(sprite, 60, -160)
-    // rcRender.setZOffset(sprite, 0)
-    music.baDing.play()
-    // sprite.setKind(SpriteKind.Food)
     otherSprite.destroy()
-    // Render.setSpriteAnimations(sprite, new Render.Animations(120, texturesCoin))
-    // sprite.setImage(sprites.builtin.coin0)
-    sprite.setScale(.5)
+    music.baDing.play()
+    tiles.placeOnRandomTile(sprite, spawnTile)
+    sprite.sayText("No!", 2000)
     sprite.startEffect(effects.fire, 3000)
-    // tiles.placeOnRandomTile(sprite, spawnTile)
-    // sprite.setVelocity(Math.randomRange(4,10), Math.randomRange(4,10))
     createCoin()
     // game.showLongText(sprite.id+" "+otherSprite.id, DialogLayout.Bottom)
 })
 
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Food, function (sprite, otherSprite) {
-    // music.pewPew.play()
-    // otherSprite.destroy()
-})
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
-    if (!rcRender.isOverlapZ(sprite, otherSprite)) return
-
     otherSprite.setVelocity(otherSprite.x - sprite.x, otherSprite.y - sprite.y)
     sprite.setVelocity(-(otherSprite.x - sprite.x), -(otherSprite.y - sprite.y))
 })
 
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    if (!rcRender.isOverlapZ(sprite, otherSprite)) return
-
-})
-
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    if (!rcRender.isOverlapZ(sprite, otherSprite)) return
-
     music.baDing.play()
     info.changeScoreBy(1)
-
-    // tiles.placeOnRandomTile(otherSprite, sprites.skillmap.islandTile4)
-    // otherSprite.setVelocity(Math.randomRange(4, 10), Math.randomRange(4, 10))
     otherSprite.destroy()
 })
 
-controller.menu.onEvent(ControllerButtonEvent.Pressed, () => {
-    Render.toggleViewMode()
+controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
+    music.pewPew.play()
+    let s = sprites.createProjectileFromSprite(sprites.projectile.bubble1, rcRender.sprSelf, 66, 0)
+    s.setScale(0.5)
 })
 
-
-
-controller.B.repeatDelay = 0
+controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
+    if(tiles.tileAtLocationIsWall(rcRender.sprSelf.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)))
+        rcRender.sprSelf.vy= -250
+    // rcRender.jumpWithHeightAndDuration(rcRender.sprSelf, tilemapScale * 2.5, 1000)
+})
 
 let isAdjusting = false
-controller.anyButton.onEvent(ControllerButtonEvent.Pressed, () => {
-    if (controller.B.isPressed()) {
-        if (controller.A.isPressed()) {
-            isAdjusting = true
-        } else {
-            if(tiles.tileAtLocationIsWall(rcRender.sprSelf.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)))
-                rcRender.sprSelf.vy= -200
-            // rcRender.jumpWithHeightAndDuration(rcRender.sprSelf, tilemapScale * 2.5, 1000)
-        }
-    }
+let adjusted=false
+controller.menu.onEvent(ControllerButtonEvent.Pressed, () => {
+    isAdjusting = true
+    adjusted=false
 })
-controller.B.onEvent(ControllerButtonEvent.Released, () => {
+controller.menu.onEvent(ControllerButtonEvent.Released, () => {
     isAdjusting = false
-})
-controller.A.onEvent(ControllerButtonEvent.Released, () => {
-    isAdjusting = false
+    if (!adjusted)
+        Render.toggleViewMode()
 })
 
 controller.up.onEvent(ControllerButtonEvent.Pressed, () => {
-    // if (isAdjusting)
-        // Render.changeScaleY(1)
+    if (!isAdjusting)
+        Render.changeScale(1)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, () => {
-    // if (isAdjusting)
-        // Render.changeScaleY(-1)
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, () => {
-    // if (isAdjusting)
-    //     Render.changeScale(-1)
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, () => {
-    // if (isAdjusting)
-    //     Render.changeScale(1)
+    if (!isAdjusting)
+        Render.changeScale(-1)
 })
 
-
-rcRender.wallZScale = 2
-
-let zOffset = 3// tilemapScale / 2
-// rcRender.setZOffset(rcRender.sprSelf, zOffset, 0)
-let fov = Render.defaultFov
 game.onUpdate(() => {
-    if (controller.up.isPressed())
-        Render.setViewAngleInDegree(--angle)
-    if (controller.down.isPressed())
-        Render.setViewAngleInDegree(++angle)
-    
-    controller.moveSprite(rcRender.sprSelf, 55 * (rcRender.viewAngle < Math.PI?1:-1), 0)
-
-    if (false && isAdjusting) {
-        // zOffset -= controller.dy(10)
-        // rcRender.setZOffset(rcRender.sprSelf, zOffset, 0)
-        fov -= controller.dy(1)
-        Render.setAttribute(Render.attribute.fov, fov)
-        info.setScore(zOffset * 100)
+    if (isAdjusting){
+        if (controller.up.isPressed()){
+            Render.changeScaleY(1)
+            adjusted = true
+        }
+        if (controller.down.isPressed()){
+            Render.changeScaleY(-1)
+            adjusted = true
+        }
+        if (controller.left.isPressed()){
+            adjusted = true
+            angle-=5
+            Render.setViewAngleInDegree(angle)
+            controller.moveSprite(rcRender.sprSelf, (rcRender.viewAngle < Math.PI ? 55 : -55), 0)
+        }
+        if (controller.right.isPressed()){
+            adjusted = true
+            angle += 5
+            Render.setViewAngleInDegree(angle)
+            controller.moveSprite(rcRender.sprSelf, (rcRender.viewAngle < Math.PI ? 55 : -55), 0)
+        }
     }
 })
 
+info.setScore(0)
+info.setLife(3)
 
-// controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
-//     for (let fov = Render.defaultFov; fov > Render.defaultFov - .6; fov -= .06) {
-//         rcRender.fov=fov
-//         pause(20)
-//     }
-// })
-// controller.B.onEvent(ControllerButtonEvent.Released, () => {
-//     for (let fov = Render.defaultFov - .6; fov <= Render.defaultFov; fov += .06) {
-//         rcRender.fov=fov
-//         pause(20)
-//     }
-// })
 
+
+
+// 0<= dir <1, then may be added by 2 for avoid negative
+// rcRender.registerOnSpriteDirectionUpdate((spr, dir) => {
+//     // character.setCharacterState(spr, character.rule(characterAniDirs[Math.floor(dir * 4 + .5) % 4]))
+// })
 
 // const characterAniDirs = [Predicate.MovingLeft, Predicate.MovingDown, Predicate.MovingRight, Predicate.MovingUp]
 // function setCharacterAnimationForSprite(spr: Sprite, textures: Image[][]) {
@@ -252,10 +146,3 @@ game.onUpdate(() => {
 //         character.loopFrames(spr, textures[Math.floor(i * textures.length / characterAniDirs.length)], 150, character.rule(dir))
 //     })
 // }
-// info.setScore(0)
-info.setLife(3)
-// let mySprite = sprites.create(sprites.builtin.computer1, SpriteKind.Player)
-// mySprite.setPosition(80,10)
-// mySprite.setFlag(SpriteFlag.RelativeToCamera, true)
-
-
